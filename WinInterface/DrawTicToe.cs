11ae03284ad7e2a8b297b02603125ef37df428b0
GarifullinAF <g.archi@yandex.ru>
2021-07-25 14:19:05 +0400

    Закончил рисование крестиков ноликов. Добавил блокировку повторного нажатия.

--- WinInterface/DrawTicToe.cs
+++ WinInterface/DrawTicToe.cs
@@ -8,6 +8,8 @@ namespace WinInterface
 {
     public class DrawTicToe
     {
+        public static int CurrentTurn = 0;
+
         public void DrawCircle (Control control)
         {
             Graphics g = control.CreateGraphics();
@@ -15,6 +17,7 @@ namespace WinInterface
             g.DrawEllipse(myPen, new Rectangle((int)myPen.Width, (int)myPen.Width, control.Width - (int)myPen.Width * 2, control.Height - (int)myPen.Width * 2));
             myPen.Dispose();
             g.Dispose();
+            CurrentTurn = 0;
         }
 
         public void DrawCross (Control control)
@@ -25,6 +28,7 @@ namespace WinInterface
             g.DrawLine(myPen, new Point((int)myPen.Width, control.Height - (int)myPen.Width), new Point(control.Width - (int)myPen.Width, (int)myPen.Width));
             myPen.Dispose();
             g.Dispose();
+            CurrentTurn = 1;
         }
     }
 }
--- WinInterface/Form1.cs
+++ WinInterface/Form1.cs
@@ -17,22 +17,95 @@ namespace WinInterface
             InitializeComponent();
         }
 
+        List<int> resultTurns = new List<int>();
+        DrawTicToe draw = new DrawTicToe();
+
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            //DrawTicToe draw = new DrawTicToe();
-            //draw.DrawCircle(pictureBox1);
-            var control = pictureBox1;
-            Graphics g = control.CreateGraphics();
-            Pen myPen = new Pen(Color.Red, 10);
-            g.DrawEllipse(myPen, new Rectangle((int)myPen.Width, (int)myPen.Width, control.Width - (int)myPen.Width * 2, control.Height - (int)myPen.Width * 2));
-            myPen.Dispose();
-            g.Dispose();
+            if (!resultTurns.Contains(1))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox1);
+                else draw.DrawCircle(pictureBox1);
+                resultTurns.Add(1);
+            }
         }
-
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            DrawTicToe draw = new DrawTicToe();
-            draw.DrawCross(pictureBox2);
+            if (!resultTurns.Contains(2))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox2);
+                else draw.DrawCircle(pictureBox2);
+                resultTurns.Add(2);
+            }
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (!resultTurns.Contains(3))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox3);
+                else draw.DrawCircle(pictureBox3);
+                resultTurns.Add(3);
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (!resultTurns.Contains(4))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox4);
+                else draw.DrawCircle(pictureBox4);
+                resultTurns.Add(4);
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (!resultTurns.Contains(5))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox5);
+                else draw.DrawCircle(pictureBox5);
+                resultTurns.Add(5);
+            }
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            if (!resultTurns.Contains(6))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox6);
+                else draw.DrawCircle(pictureBox6);
+                resultTurns.Add(6);
+            }
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            if (!resultTurns.Contains(7))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox7);
+                else draw.DrawCircle(pictureBox7);
+                resultTurns.Add(7);
+            }
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            if (!resultTurns.Contains(8))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox8);
+                else draw.DrawCircle(pictureBox8);
+                resultTurns.Add(8);
+            }
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            if (!resultTurns.Contains(9))
+            {
+                if (DrawTicToe.CurrentTurn == 0) draw.DrawCross(pictureBox9);
+                else draw.DrawCircle(pictureBox9);
+                resultTurns.Add(9);
+            }
         }
     }
 }
